e4fcfbf4f17ca287ff722a0d26b2abb5b8069990
Stiaan Wolfaardt <stiaan99@gmail.com>
2023-09-30 17:03:07 +0200

    remove root content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import { ModeToggle } from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
 
 export const dynamic = "force-dynamic";
 
@@ -22,22 +19,5 @@ export default async function Index() {
     redirect("/sign-in");
   }
 
-  return (
-    <div className="w-full h-full flex flex-col items-center">
-      <nav className="w-full flex justify-center h-16">
-        <div className="w-full flex justify-between items-center p-3 text-sm text-foreground">
-          <div />
-          <div className="flex gap-x-4">
-            <ModeToggle />
-            <Button variant="outline" asChild>
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button variant="outline" asChild>
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+  return <></>;
 }
